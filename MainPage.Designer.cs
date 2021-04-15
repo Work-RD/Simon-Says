fbdc5af53aa6e83abc8352d2fcbb310c4ac2ca0c
Work-RD <68544754+Work-RD@users.noreply.github.com>
2021-04-15 09:44:29 +0300

    Add project files.

---
+++ MainPage.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Simon_Says
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
+            this.ask_txt = new System.Windows.Forms.TextBox();
+            this.question_txt = new System.Windows.Forms.TextBox();
+            this.title_lbl = new System.Windows.Forms.Label();
+            this.ask_lbl = new System.Windows.Forms.Label();
+            this.question_lbl = new System.Windows.Forms.Label();
+            this.howto_btn = new System.Windows.Forms.Button();
+            this.warning_pic = new System.Windows.Forms.PictureBox();
+            this.warning_tip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.warning_pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ask_txt
+            // 
+            this.ask_txt.Location = new System.Drawing.Point(12, 94);
+            this.ask_txt.Name = "ask_txt";
+            this.ask_txt.Size = new System.Drawing.Size(352, 20);
+            this.ask_txt.TabIndex = 0;
+            this.ask_txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ask_txt_KeyPress);
+            // 
+            // question_txt
+            // 
+            this.question_txt.Location = new System.Drawing.Point(12, 160);
+            this.question_txt.Name = "question_txt";
+            this.question_txt.Size = new System.Drawing.Size(352, 20);
+            this.question_txt.TabIndex = 1;
+            this.question_txt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.question_txt_KeyDown);
+            // 
+            // title_lbl
+            // 
+            this.title_lbl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.title_lbl.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_lbl.Location = new System.Drawing.Point(0, 0);
+            this.title_lbl.Name = "title_lbl";
+            this.title_lbl.Size = new System.Drawing.Size(376, 51);
+            this.title_lbl.TabIndex = 2;
+            this.title_lbl.Text = "Simon Says";
+            this.title_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ask_lbl
+            // 
+            this.ask_lbl.AutoSize = true;
+            this.ask_lbl.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ask_lbl.Location = new System.Drawing.Point(12, 62);
+            this.ask_lbl.Name = "ask_lbl";
+            this.ask_lbl.Size = new System.Drawing.Size(308, 29);
+            this.ask_lbl.TabIndex = 3;
+            this.ask_lbl.Text = "Request Simon to answer you. ";
+            // 
+            // question_lbl
+            // 
+            this.question_lbl.AutoSize = true;
+            this.question_lbl.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.question_lbl.Location = new System.Drawing.Point(12, 128);
+            this.question_lbl.Name = "question_lbl";
+            this.question_lbl.Size = new System.Drawing.Size(254, 29);
+            this.question_lbl.TabIndex = 4;
+            this.question_lbl.Text = "Your question for Simon.";
+            // 
+            // howto_btn
+            // 
+            this.howto_btn.ForeColor = System.Drawing.Color.Black;
+            this.howto_btn.Location = new System.Drawing.Point(309, 201);
+            this.howto_btn.Name = "howto_btn";
+            this.howto_btn.Size = new System.Drawing.Size(55, 23);
+            this.howto_btn.TabIndex = 5;
+            this.howto_btn.Text = "How to?";
+            this.howto_btn.UseVisualStyleBackColor = true;
+            this.howto_btn.Click += new System.EventHandler(this.howto_btn_Click);
+            // 
+            // warning_pic
+            // 
+            this.warning_pic.ErrorImage = null;
+            this.warning_pic.Image = ((System.Drawing.Image)(resources.GetObject("warning_pic.Image")));
+            this.warning_pic.InitialImage = null;
+            this.warning_pic.Location = new System.Drawing.Point(8, 188);
+            this.warning_pic.Name = "warning_pic";
+            this.warning_pic.Size = new System.Drawing.Size(59, 36);
+            this.warning_pic.TabIndex = 6;
+            this.warning_pic.TabStop = false;
+            this.warning_pic.MouseHover += new System.EventHandler(this.warning_pic_MouseHover);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(376, 236);
+            this.Controls.Add(this.warning_pic);
+            this.Controls.Add(this.howto_btn);
+            this.Controls.Add(this.question_lbl);
+            this.Controls.Add(this.ask_lbl);
+            this.Controls.Add(this.title_lbl);
+            this.Controls.Add(this.question_txt);
+            this.Controls.Add(this.ask_txt);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simon Says";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainPage_FormClosing);
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.warning_pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ask_txt;
+        private System.Windows.Forms.TextBox question_txt;
+        private System.Windows.Forms.Label title_lbl;
+        private System.Windows.Forms.Label ask_lbl;
+        private System.Windows.Forms.Label question_lbl;
+        private System.Windows.Forms.Button howto_btn;
+        private System.Windows.Forms.PictureBox warning_pic;
+        private System.Windows.Forms.ToolTip warning_tip;
+    }
+}
+
---
+++ MainPage.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Simon_Says
+{
+    public partial class MainPage : Form
+    {
+        public int stack = 0;
+        public int stack1 = 0;
+        public int stack2 = 0;
+        public string message = "";
+        public bool answer = false;
+        public bool dash = false;
+        public string cmd = "NU";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+            stack = 0;
+            stack1 = 0;
+            message = "";
+            answer = false;
+            dash = false;
+            ask_txt.Text = "";
+            question_txt.Text = "";
+            ask_txt.ReadOnly = false;
+            ask_txt.Focus();
+            warning_tip.SetToolTip(warning_pic, "We are not responsible for your questions! \nYou use the application at your own risk!");
+        }
+
+        private void MainPage_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (cmd == "DA")
+            {
+                SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+                speechSynthesizer.Speak("Bye Bye.");
+                speechSynthesizer.Dispose();
+                Environment.Exit(0);
+            }
+            else
+            {
+                e.Cancel = true;
+                SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+                speechSynthesizer.Speak("Simon told you that you use the application at your own risk.");
+                speechSynthesizer.Dispose();
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.Shift | Keys.R))
+            {
+                cmd = "DA";
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void question_txt_KeyDown(object sender, KeyEventArgs e)
+        { 
+            if (e.KeyCode == Keys.Enter)
+            {
+                DialogResult dialog;
+                if (dash == true && (ask_txt.Text == "Simon pretty please" || ask_txt.Text == "Simon pretty please tell me the answer"))
+                {
+                    message = message.Substring(1);
+                    SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+                    speechSynthesizer.Speak("Simon says" + message);
+                    speechSynthesizer.Dispose();
+                    dialog = MessageBox.Show(message, "Simon Says", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (dialog == DialogResult.OK)
+                    {
+                        MainPage_Load(null, EventArgs.Empty);
+                    }
+                }
+                else
+                {
+                    var list = new List<string> { "I don't have time for your questions.", "Not at your service.", "Try to read the manual.", "I think you are joking.", "I don't want to answer that." };
+                    var random = new Random();
+                    int index = random.Next(list.Count);
+
+                    SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+                    speechSynthesizer.Speak("Simon says" + list[index]);
+                    speechSynthesizer.Dispose();
+                    dialog = MessageBox.Show(list[index], "Simon Says", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (dialog == DialogResult.OK)
+                    {
+                        MainPage_Load(null, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        private void ask_txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            stack++;
+            if (e.KeyChar == '/')
+            {
+                stack1++;
+                switch (stack1)
+                {
+                    case 1:
+                        answer = true;
+                        dash = true;
+                        ask_txt.ReadOnly = true;
+                        ask_txt.BackColor = System.Drawing.SystemColors.Window;
+                        break;
+                    case 2:
+                        answer = false;
+                        ask_txt.ReadOnly = false;
+                        e.Handled = true;
+                        break;
+                }
+            }
+            if (answer == true)
+            {
+                switch (stack)
+                {
+                    case 1:
+                        ask_txt.Text += "S";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 2:
+                        ask_txt.Text += "i";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 3:
+                        ask_txt.Text += "m";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 4:
+                        ask_txt.Text += "o";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 5:
+                        ask_txt.Text += "n";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 6:
+                        ask_txt.Text += " ";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 7:
+                        ask_txt.Text += "p";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 8:
+                        ask_txt.Text += "r";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 9:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 10:
+                        ask_txt.Text += "t";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 11:
+                        ask_txt.Text += "t";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 12:
+                        ask_txt.Text += "y";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 13:
+                        ask_txt.Text += " ";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 14:
+                        ask_txt.Text += "p";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 15:
+                        ask_txt.Text += "l";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 16:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 17:
+                        ask_txt.Text += "a";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 18:
+                        ask_txt.Text += "s";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 19:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 20:
+                        ask_txt.Text += " ";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 21:
+                        ask_txt.Text += "t";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 22:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 23:
+                        ask_txt.Text += "l";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 24:
+                        ask_txt.Text += "l";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 25:
+                        ask_txt.Text += " ";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 26:
+                        ask_txt.Text += "m";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 27:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 28:
+                        ask_txt.Text += " ";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 29:
+                        ask_txt.Text += "t";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 30:
+                        ask_txt.Text += "h";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 31:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 32:
+                        ask_txt.Text += " ";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 33:
+                        ask_txt.Text += "a";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 34:
+                        ask_txt.Text += "n";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 35:
+                        ask_txt.Text += "s";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 36:
+                        ask_txt.Text += "w";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 37:
+                        ask_txt.Text += "e";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                    case 38:
+                        ask_txt.Text += "r";
+                        ask_txt.SelectionStart = ask_txt.Text.Length;
+                        message = message + e.KeyChar.ToString();
+                        break;
+                }
+                if (stack > 38)
+                {
+                    message = message + e.KeyChar.ToString();
+                }
+            }
+        }
+
+        private void howto_btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Simon is very busy. In order to reach to you, and get your answer. You have to request him. \n\nYou can do this by saying this: \n-Simon pretty please \n\nor this: \n-Simon pretty please tell me the answer \n\nThen add your question and press enter.", "How to get Simon answer your question?", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        private void warning_pic_MouseHover(object sender, EventArgs e)
+        {
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.Speak("Warning");
+            speechSynthesizer.Dispose();
+        }
+    }
+}
